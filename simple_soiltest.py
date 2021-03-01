d7e5c6bc4f403d4df19bba8e92c79aa628e6206a
caitlinbrooks03 <iq4186rf@go.minnstate.edu>
2021-03-01 20:33:49 +0000

    updated soiltest

--- simple_soiltest.py
+++ simple_soiltest.py
@@ -1,3 +1,4 @@
+
 import time
 
 from board import SCL, SDA
@@ -5,22 +6,33 @@ import busio
 
 from adafruit_seesaw.seesaw import Seesaw
 
-i2c_bus = busio.I2C(SCL, SDA)
 
-ss = Seesaw(i2c_bus, addr=0x36)
+if __name__ == "__main__":
 
-while True:
-	# read moisture level through capacitive touch pad
-	touch = ss.moisture_read()
+	i2c_bus = busio.I2C(SCL, SDA)
 
-	#read temperature from temperature sensor
-	celcius_temp = ss.get_temp()
-	fahrenheit_temp = (celcius_temp * (9/5)) + 32
+	ss = Seesaw(i2c_bus, addr=0x36)
+	print("Room: Bedroom")
+	print("Plant: Pilea Baby Tears\n")
+
+	while True:
+		# read moisture level through capacitive touch pad
+		touch = ss.moisture_read()
 	
-	if(touch < 600):
-		print("Needs more Water")
-	elif (touch > 1500):
-		print("Too Wet")
-	else:
-		print("Temp (F): " + str(fahrenheit_temp) + "\n Moisture: " + str(touch))
-	time.sleep(1)
+		#read temperature from temperature sensor
+		celcius_temp = ss.get_temp()
+		fahrenheit_temp = (celcius_temp * (9/5)) + 32
+		
+		if (touch < 600):
+			print("Needs More Water")
+			print("Current Temp (F): " + str(fahrenheit_temp) + " Moisture: " + str(touch))
+		elif (touch > 1500):
+			print("Too Much Water")
+			print("Current Temp (F): " + str(fahrenheit_temp) + " Moisture: " + str(touch))
+
+		else:
+			print("Temp (F): " + str(fahrenheit_temp) + " Moisture: " + str(touch))
+		time.sleep(1)
+
+
+
